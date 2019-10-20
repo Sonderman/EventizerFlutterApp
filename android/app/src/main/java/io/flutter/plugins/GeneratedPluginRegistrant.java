2c250689668c289910618f4ae249f8ca8813b881
Sonderman <alihaydar338@gmail.com>
2019-10-20 16:19:25 +0300

    Üye kayıt,giriş,çıkış ve denetim denemesi yapıldı , android paketleri güncellendi , yönlendirmede sıkıntı var.

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,6 +1,9 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.firebaseanalytics.FirebaseAnalyticsPlugin;
+import io.flutter.plugins.firebaseauth.FirebaseAuthPlugin;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
 
 /**
  * Generated file. Do not edit.
@@ -10,6 +13,9 @@ public final class GeneratedPluginRegistrant {
     if (alreadyRegisteredWith(registry)) {
       return;
     }
+    FirebaseAnalyticsPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebaseanalytics.FirebaseAnalyticsPlugin"));
+    FirebaseAuthPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebaseauth.FirebaseAuthPlugin"));
+    FirebaseCorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.core.FirebaseCorePlugin"));
   }
 
   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
---
+++ lib/Root_page.dart
@@ -0,0 +1,52 @@
+import 'Home_page.dart';
+import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+import 'Auth.dart';
+
+class RootPage extends StatefulWidget {
+  RootPage({this.auth});
+  final BaseAuth auth;
+  @override
+  State<StatefulWidget> createState() => _RootPageState();
+
+}
+enum AuthStatus{
+  SignedIn,
+  NotSignetIn
+}
+class _RootPageState extends State<RootPage>{
+  AuthStatus authStatus = AuthStatus.NotSignetIn;
+@override
+  initState() {
+    super.initState();
+    widget.auth.currentUser().then((userID){
+      setState(() {
+        authStatus = userID == null ? AuthStatus.NotSignetIn : AuthStatus.SignedIn;
+      });
+    });
+  }
+
+  void _signedIn(){
+setState(() {
+  authStatus = AuthStatus.SignedIn;
+});
+}
+  void _signedOut() {
+    setState(() {
+      authStatus = AuthStatus.NotSignetIn;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    switch(authStatus){
+      case AuthStatus.NotSignetIn:
+        return LoginPage(auth: widget.auth,onSignedIn: _signedIn,);
+
+      case AuthStatus.SignedIn :
+        return HomePage(
+          auth: widget.auth,
+          onSignedOut: _signedOut,
+        );
+    }
+  }
+}
