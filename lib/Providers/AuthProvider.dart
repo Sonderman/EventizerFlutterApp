e20e0abc2a07493ea394b253e9d7ab5ad48968bd
Sonderman <alihaydar338@gmail.com>
2019-12-17 14:11:56 +0300

    Deprecated inherited auth düzeltildi.

--- lib/Providers/AuthProvider.dart
+++ lib/Providers/AuthProvider.dart
@@ -1,17 +1,18 @@
 import 'package:eventizer/Services/BaseAuth.dart';
 import 'package:flutter/material.dart';
 
-
-class AuthProvider extends InheritedWidget{
-  AuthProvider({Key key,this.auth, Widget child}) : super(key: key, child: child);
+class AuthProvider extends InheritedWidget {
+  AuthProvider({Key key, this.auth, Widget child})
+      : super(key: key, child: child);
   final BaseAuth auth;
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
-    
     return true;
   }
-static AuthProvider of(BuildContext context){
-  return (context.inheritFromWidgetOfExactType(AuthProvider) as AuthProvider);
-}
+
+  static AuthProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<AuthProvider>();
+    // return (context.inheritFromWidgetOfExactType(AuthProvider) as AuthProvider);
+  }
 }
